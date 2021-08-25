5c2bd217e19664894b8e7f577e708575a75cf2c8
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-08-25 21:34:55 +0100

    Files 5

---
+++ Ball Goes Roll/Assets/Triggers/Scripts/TouchTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTrigger : Trigger {
+    protected override void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(triggeredBy) && isActivatable) {
+            onTriggerMethod.Invoke();
+            base.OnTriggerEnter(other);
+        }
+    }
+}
---
+++ Ball Goes Roll/Assets/Triggers/Scripts/Trigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Trigger : MonoBehaviour
+{
+    [SerializeField] protected bool activateOnce = true;
+    [SerializeField] protected bool isActivatable = true;
+
+    [SerializeField] protected UnityEvent onTriggerMethod;
+    [SerializeField] protected string triggeredBy = "Player";
+
+    public bool IsActivatable {
+        get { return isActivatable; }
+        set { isActivatable = value; }
+    }
+
+
+
+    protected virtual void OnTriggerEnter(Collider other) {
+        if (activateOnce) {
+            Destroy(this.gameObject);
+        }
+    }
+}
